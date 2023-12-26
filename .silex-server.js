bad07506776ee3a1297c1a833fddab15bf787030
Alex Hoyau <a.hoyau@silexlabs.org>
2023-12-26 09:49:03 +0300

    new name for silex cms

--- .silex-server.js
+++ .silex-server.js
@@ -1,12 +1,12 @@
 const StaticPlugin = require('@silexlabs/silex/dist/plugins/server/plugins/server/StaticPlugin').default
 const node_modules = require('node_modules-path')
-console.log('node_modules', node_modules('@silexlabs/silex-plugin-11ty'))
+console.log('node_modules', node_modules('@silexlabs/silex-cms'))
 module.exports = function(config, options) {
   config.addPlugin(StaticPlugin, {
     routes: [
       {
-        route: '/js/silex-plugin-11ty/',
-        path: node_modules('@silexlabs/silex-plugin-11ty') + '/@silexlabs/silex-plugin-11ty/dist/',
+        route: '/js/silex-cms/',
+        path: node_modules('@silexlabs/silex-cms') + '/@silexlabs/silex-cms/dist/',
       },
     ],
   })
--- .silex.js
+++ .silex.js
@@ -1,4 +1,4 @@
-import Eleventy from './js/silex-plugin-11ty/client.js'
+import Eleventy from './js/silex-cms/client.js'
 export default function(config, options) {
   config.addPlugin(Eleventy, {
     dataSources: [{
